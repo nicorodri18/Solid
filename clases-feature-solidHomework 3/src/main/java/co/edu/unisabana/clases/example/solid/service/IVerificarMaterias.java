8fe22ec3b62e5151824b3da1a96c6ffc07919edc
nicorodri18 <100327214+nicorodri18@users.noreply.github.com>
2024-02-04 22:05:10 -0500

    Delete clases-feature-solidHomework 3 directory

--- clases-feature-solidHomework 3/src/main/java/co/edu/unisabana/clases/example/solid/service/IVerificarMaterias.java
+++
@@ -1,9 +0,0 @@
-package co.edu.unisabana.clases.example.solid.service;
-
-import co.edu.unisabana.clases.example.solid.modelo.Estudiante;
-
-public interface IVerificarMaterias {
-    boolean validarCarrera(Estudiante estudiante);
-
-    String MateriasCarrera();
-}
